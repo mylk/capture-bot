e23682d487d2a326e95fb266f9ade14fb6135c17
Kostas Milonas <milonas.ko@gmail.com>
2017-02-19 12:39:36 +0200

    refactors application options

--- capture-bot/main.py
+++ capture-bot/main.py
@@ -12,14 +12,22 @@ from datetime import datetime
 import json
 import re
 
-arguments = OptionParser()
-arguments.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Verbose mode")
-options = arguments.parse_args()[0]
-
 config = ConfigParser()
 config.read("capture_bot.cfg")
-subreddits = json.loads(config.get("Default", "subreddits"))
-domain_names = json.loads(config.get("Default", "domain_names"))
+options = {}
+options.update({ "version": config.get("Generic", "version" )})
+options.update({ "verbose": config.getboolean("Generic", "verbose") })
+options.update({ "persist_parsed_threads": config.getboolean("Generic", "persist_parsed_threads") })
+options.update({ "watched_subreddits": json.loads(config.get("Capture", "watched_subreddits")) })
+options.update({ "captured_domain_names": json.loads(config.get("Capture", "captured_domain_names")) })
+options.update({ "dump_directory": config.get("Capture", "dump_directory") })
+
+arguments = OptionParser()
+arguments.add_option("-v", "--verbose", action="store_true", dest="verbose", help="Verbose mode")
+arguments.add_option("-p", "--persist", action="store_true", dest="persist_parsed_threads", help="Persist parsed threads")
+# merge with ConfigParser but let OptionParser win
+arguments.set_defaults(**options)
+options = arguments.parse_args()[0]
 
 logger = Logger(options.verbose)
 reddit = RedditClient()
@@ -30,7 +38,7 @@ database = Database()
 print datetime.now().strftime("Started at %Y-%m-%d %H:%M:%S.")
 
 # fetch the latest reddit posts and comments of given subreddits
-reddit.fetch_posts(subreddits)
+reddit.fetch_posts(options.watched_subreddits)
 posts = reddit.get_posts()
 
 # loop through all results and check for links of the given domains
@@ -39,15 +47,16 @@ for post in posts:
         logger.write_console("Element %s already parsed" % (post["id"]))
         continue
     else:
-        logger.write_console("Element %s WASN't parsed" % (post["id"]))
-        database.store_element(post)
+        logger.write_console("Element %s NOW parsed" % (post["id"]))
+        if options.persist_parsed_threads:
+            database.store_element(post)
 
-    urls = inspector.find_domain_urls(post, domain_names)
+    urls = inspector.find_domain_urls(post, options.captured_domain_names)
 
     # capture all urls containing the domain name
     for url in urls:
         # sanitize the url, to use it as a filename
         filename = re.sub("[^0-9a-zA-Z]", "-", url)
-        capturer.capture(url, "/tmp/%s.png" % (filename))
+        capturer.capture(url, "%s/%s.png" % (options.dump_directory, filename))
 
 print datetime.now().strftime("Ended at %Y-%m-%d %H:%M:%S.")

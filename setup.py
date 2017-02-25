642d7cc6d45af2e05229b227bb1c64246483dcc7
Kostas Milonas <milonas.ko@gmail.com>
2017-02-25 20:51:37 +0200

    adds setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python2
+from distutils.core import setup
+
+setup(
+    name="capturebot",
+    version="0.1",
+    description="A Reddit bot that captures and posts screenshots of websites referenced in posts or comments",
+    author="Kostas Milonas",
+    author_email="milonas.ko@gmail.com",
+    packages=["capturebot"]
+)
